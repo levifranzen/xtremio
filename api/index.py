46c19308e6323db8656c313c2f8abb25494895c5
Levi Augusto Franzen <35925385+levifranzen@users.noreply.github.com>
2026-05-24 21:41:25 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -80,7 +80,7 @@ def normalize_string(s):
     s = "".join(
         c for c in unicodedata.normalize("NFD", s) if unicodedata.category(c) != "Mn"
     ).lower()
-    s = re.sub(r"[^a-z0-9\s]", " ", s)
+    s = re.sub(r"[^a-z0-9\s]", "", s)
     s = re.sub(r"\s+", " ", s)
     return s.strip()
 
