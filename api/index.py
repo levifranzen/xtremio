c5a83080d69362ab786cf5727d695ff91dbeb756
Levi Augusto Franzen <35925385+levifranzen@users.noreply.github.com>
2026-05-26 11:36:12 -0300

    Adicionado substituição de & por e

--- api/index.py
+++ api/index.py
@@ -82,6 +82,7 @@ def normalize_string(s):
         c for c in unicodedata.normalize("NFD", s) if unicodedata.category(c) != "Mn"
     ).lower()
     s = re.sub(r"[^a-z0-9\s]", "", s)
+    s = re.sub(r"&", "e", s)
     s = re.sub(r"\s+", " ", s)
     return s.strip()
 
